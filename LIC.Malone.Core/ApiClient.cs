fa61de21d3a4b9094a52f220646207758d7a24e8
Shannon Skinner <shannonsnz@gmail.com>
2015-03-11 14:27:37 +1300

    Stubbing out solution structure.

---
+++ LIC.Malone.Core/ApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIC.Malone.Core
+{
+    public class ApiClient
+    {
+        public bool Send(Request request)
+        {
+            return false;
+        }
+    }
+}
---
+++ LIC.Malone.Core/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIC.Malone.Core
+{
+    public class Request
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+    }
+}
