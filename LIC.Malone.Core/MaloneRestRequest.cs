154263f1292beac2aaf4dafed795b34551d98544
Shannon Skinner <shannonsnz@gmail.com>
2015-03-18 10:12:57 +1300

    WIP

--- LIC.Malone.Core/MaloneRestRequest.cs
+++ LIC.Malone.Core/MaloneRestRequest.cs
@@ -7,36 +7,13 @@ namespace LIC.Malone.Core
 	{
 		public Uri AbsoluteUrl { get; private set; }
 		public string BaseUrl { get; private set; }
-		public string ResourceUrl { get; private set; }
+		public string ResourcePath { get; private set; }
 
-		public MaloneRestRequest(Uri url, Method method) : this(url, ToBaseUrl(url), ToResourceUrl(url), method)
-		{
-		}
-
-		private MaloneRestRequest(Uri absoluteUrl, string baseUrl, string resourceUrl, Method method) : base(resourceUrl, method)
+		public MaloneRestRequest(Uri absoluteUrl, string baseUrl, string resourcePath, Method method) : base(resourcePath, method)
 		{
 			AbsoluteUrl = absoluteUrl;
 			BaseUrl = baseUrl;
-			ResourceUrl = resourceUrl;
-		}
-
-		private static string ToBaseUrl(Uri url)
-		{
-			var baseUrl = new UriBuilder(url.Scheme, url.Host, url.Port).ToString();
-
-			// Remove trailing slash to keep RestSharp happy.
-			return baseUrl.Substring(0, baseUrl.Length - 1);
-		}
-
-		private static string ToResourceUrl(Uri url)
-		{
-			var path = url.AbsolutePath;
-
-			// Remove leading slash to keep RestSharp happy.
-			if (path.StartsWith("/"))
-				path = path.Substring(1);
-
-			return path;
+			ResourcePath = resourcePath;
 		}
 	}
 }
--- LIC.Malone.Core/Request.cs
+++ LIC.Malone.Core/Request.cs
@@ -9,6 +9,16 @@ namespace LIC.Malone.Core
 		public string Method { get; set; }
 		public string Token { get; set; }
 
+		public string BaseUrl
+		{
+			get { return GetBaseUrl(); }
+		}
+
+		public string ResourcePath
+		{
+			get { return GetResourcePath(); }
+		}
+
 		private Method GetRestSharpMethod()
 		{
 			switch (Method)
@@ -22,17 +32,56 @@ namespace LIC.Malone.Core
 
 		public MaloneRestRequest ToMaloneRestRequest()
 		{
-			Uri url;
-			if (!Uri.TryCreate(Url, UriKind.Absolute, out url))
-				throw new Exception(string.Format("Failed to create a URI from the string '{0}'.", Url));
+			var url = GetUri();
+			var baseUrl = GetBaseUrl(url);
+			var resourcePath = GetResourcePath(url);
+			var method = GetRestSharpMethod();
 			
-			var request = new MaloneRestRequest(url, GetRestSharpMethod());
+			var request = new MaloneRestRequest(url, baseUrl, resourcePath, method);
 
 			request.AddHeader("Accept", "text/xml");
 			request.AddHeader("Accept-Encoding", "gzip,deflate");
 			request.AddHeader("Authorization", string.Concat("Bearer ", Token));
 
 			return request;
+		}
+
+		private Uri GetUri()
+		{
+			Uri url;
+			if (!Uri.TryCreate(Url, UriKind.Absolute, out url))
+				throw new Exception(string.Format("Failed to create a URI from the string '{0}'.", Url));
+
+			return url;
+		}
+
+		private string GetBaseUrl()
+		{
+			return GetBaseUrl(GetUri());
+		}
+
+		private string GetBaseUrl(Uri url)
+		{
+			var baseUrl = new UriBuilder(url.Scheme, url.Host, url.Port).ToString();
+
+			// Remove trailing slash to keep RestSharp happy.
+			return baseUrl.Substring(0, baseUrl.Length - 1);
+		}
+
+		private string GetResourcePath()
+		{
+			return GetResourcePath(GetUri());
+		}
+
+		private string GetResourcePath(Uri url)
+		{
+			var path = url.AbsolutePath;
+
+			// Remove leading slash to keep RestSharp happy.
+			if (path.StartsWith("/"))
+				path = path.Substring(1);
+
+			return path;
 		}
 	}
 }
