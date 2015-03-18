c3ac9e5a32beb008e7e6aac42505b7d6d7f17f8d
Shannon Skinner <shannonsnz@gmail.com>
2015-03-19 12:08:06 +1300

    Tidy

---
+++ LIC.Malone.Client.Desktop/Extensions/HttpStatusCodeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Windows.Media;
+
+namespace LIC.Malone.Client.Desktop.Extensions
+{
+	public static class HttpStatusCodeExtensions
+	{
+		public static Brush ToBrush(this HttpStatusCode code)
+		{
+			var ok = new SolidColorBrush(Color.FromRgb(4, 150, 4));
+			var maybe = new SolidColorBrush(Color.FromRgb(245, 113, 25));
+			var nope = new SolidColorBrush(Color.FromRgb(235, 40, 34));
+
+			switch (code)
+			{
+				case HttpStatusCode.OK:
+					return ok;
+
+				case HttpStatusCode.Accepted:
+					return ok;
+
+				case HttpStatusCode.NoContent:
+					return maybe;
+
+				case HttpStatusCode.Created:
+					return maybe;
+
+				default:
+					return nope;
+			}
+		}
+	}
+}
--- LIC.Malone.Client.Desktop/ViewModels/HttpStatusCodeViewModel.cs
+++ LIC.Malone.Client.Desktop/ViewModels/HttpStatusCodeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Web;
 using System.Windows.Media;
+using LIC.Malone.Client.Desktop.Extensions;
 
 namespace LIC.Malone.Client.Desktop.ViewModels
 {
@@ -14,27 +15,7 @@ namespace LIC.Malone.Client.Desktop.ViewModels
 		{
 			Code = (int) code;
 			Description = HttpWorkerRequest.GetStatusDescription(Code);
-			CodeColor = GetBackgroundColor(code);
-
-		}
-
-		private SolidColorBrush GetBackgroundColor(HttpStatusCode code)
-		{
-			var ok = new SolidColorBrush(Color.FromRgb(4, 150, 4));
-			var maybe = new SolidColorBrush(Color.FromRgb(245, 113, 25));
-			var nope = new SolidColorBrush(Color.FromRgb(235, 40, 34));
-
-			switch (code)
-			{
-				case HttpStatusCode.OK:
-					return ok;
-
-				case HttpStatusCode.NoContent:
-					return maybe;
-
-				default:
-					return nope;
-			}
+			CodeColor = code.ToBrush();
 		}
 	}
 }
