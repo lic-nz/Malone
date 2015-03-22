9b539db8b2bdf29ec55b7d8ce6ee76b35605628d
Shannon Skinner <shannonsnz@gmail.com>
2015-03-23 11:54:07 +1300

    WIP

---
+++ LIC.Malone.Client.Desktop/MyDependencyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LIC.Malone.Client.Desktop
+{
+	public class MyDependencyClass : DependencyObject
+	{
+		#region IsCheckedOnDataProperty
+
+		public static readonly DependencyProperty DataForPathProperty;
+
+		public static void SetDataForPath(DependencyObject DepObject, Canvas value)
+		{
+			DepObject.SetValue(DataForPathProperty, value);
+		}
+
+		public static Canvas GetDataForPath(DependencyObject DepObject)
+		{
+			return (Canvas)DepObject.GetValue(DataForPathProperty);
+		}
+
+		#endregion
+
+		static MyDependencyClass()
+		{
+			DataForPathProperty = DependencyProperty.RegisterAttached("DataForPath",
+				typeof(Canvas),
+				typeof(MyDependencyClass),
+				new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+		}
+	}
+}
