4f53e5c0bb5159ef59f2e46060fa9e2865d09075
Shannon Skinner <shannonsnz@gmail.com>
2015-03-23 13:00:51 +1300

    WIP

---
+++ LIC.Malone.Client.Desktop/HyperlinkButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LIC.Malone.Client.Desktop
+{
+	public class HyperlinkButton : DependencyObject
+	{
+		#region IsCheckedOnDataProperty
+
+		public static readonly DependencyProperty IconProperty;
+
+		public static void SetIcon(DependencyObject obj, PathGeometry value)
+		{
+			obj.SetValue(IconProperty, value);
+		}
+
+		public static PathGeometry GetIcon(DependencyObject obj)
+		{
+			return (PathGeometry)obj.GetValue(IconProperty);
+		}
+
+		#endregion
+
+		static HyperlinkButton()
+		{
+			IconProperty = DependencyProperty.RegisterAttached("Icon",
+				typeof(PathGeometry),
+				typeof(HyperlinkButton),
+				new FrameworkPropertyMetadata(null));
+		}
+	}
+}
