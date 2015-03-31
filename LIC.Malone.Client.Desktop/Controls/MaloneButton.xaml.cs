9bb7f5f436220b45b283fad902655b16f34f677d
Shannon Skinner <shannonsnz@gmail.com>
2015-03-31 16:45:23 +1300

    Allow better placement of button icons.

--- LIC.Malone.Client.Desktop/Controls/MaloneButton.xaml.cs
+++ LIC.Malone.Client.Desktop/Controls/MaloneButton.xaml.cs
@@ -13,6 +13,20 @@ namespace LIC.Malone.Client.Desktop.Controls
 			set { SetValue(IconDataProperty, value); }
 		}
 
+		public static readonly DependencyProperty IconTopProperty = DependencyProperty.Register("IconTop", typeof(double), typeof(MaloneButton), new PropertyMetadata((double)-1));
+		public double IconTop
+		{
+			get { return (double)GetValue(IconTopProperty); }
+			set { SetValue(IconTopProperty, value); }
+		}
+
+		public static readonly DependencyProperty TextMarginProperty = DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(MaloneButton), new PropertyMetadata(new Thickness(20, 0, 0, 0)));
+		public Thickness TextMargin
+		{
+			get { return (Thickness)GetValue(TextMarginProperty); }
+			set { SetValue(TextMarginProperty, value); }
+		}
+
 		public MaloneButton()
 		{
 			InitializeComponent();
