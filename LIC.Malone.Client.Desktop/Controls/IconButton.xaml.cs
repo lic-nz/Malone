10a29e423922418c9ef6dac8e2a2700f176b0e39
stajs <shannonsnz@gmail.com>
2015-04-01 22:15:06 +1300

    Adding links as secondary actions.

---
+++ LIC.Malone.Client.Desktop/Controls/IconButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LIC.Malone.Client.Desktop.Controls
+{
+	public partial class IconButton : Button
+	{
+		public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register("IconData", typeof(Geometry), typeof(IconButton), new PropertyMetadata());
+		public Geometry IconData
+		{
+			get { return (Geometry)GetValue(IconDataProperty); }
+			set { SetValue(IconDataProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconTopProperty = DependencyProperty.Register("IconTop", typeof(double), typeof(IconButton), new PropertyMetadata((double)-1));
+		public double IconTop
+		{
+			get { return (double)GetValue(IconTopProperty); }
+			set { SetValue(IconTopProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconMarginProperty = DependencyProperty.Register("IconMargin", typeof(Thickness), typeof(IconButton), new PropertyMetadata());
+		public Thickness IconMargin
+		{
+			get { return (Thickness)GetValue(IconMarginProperty); }
+			set { SetValue(IconMarginProperty, value); }
+		}
+
+		public IconButton()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ LIC.Malone.Client.Desktop/Controls/IconLink.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LIC.Malone.Client.Desktop.Controls
+{
+	public partial class IconLink : Button
+	{
+		public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register("IconData", typeof(Geometry), typeof(IconLink), new PropertyMetadata());
+		public Geometry IconData
+		{
+			get { return (Geometry)GetValue(IconDataProperty); }
+			set { SetValue(IconDataProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconTopProperty = DependencyProperty.Register("IconTop", typeof(double), typeof(IconLink), new PropertyMetadata((double)-1));
+		public double IconTop
+		{
+			get { return (double)GetValue(IconTopProperty); }
+			set { SetValue(IconTopProperty, value); }
+		}
+
+		public static readonly DependencyProperty TextMarginProperty = DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(IconLink), new PropertyMetadata(new Thickness(-4, -4, 0, 0)));
+		public Thickness TextMargin
+		{
+			get { return (Thickness)GetValue(TextMarginProperty); }
+			set { SetValue(TextMarginProperty, value); }
+		}
+
+		public IconLink()
+		{
+			InitializeComponent();
+		}
+	}
+}
