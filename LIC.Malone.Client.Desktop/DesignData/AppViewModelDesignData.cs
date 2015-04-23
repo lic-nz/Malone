829ad897adfd33cdaf32e37358dc7a54d2dff8c1
stajs <shannonsnz@gmail.com>
2015-04-23 23:35:10 +1200

    Tooling around with design time data.

---
+++ LIC.Malone.Client.Desktop/DesignData/AppViewModelDesignData.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using LIC.Malone.Client.Desktop.ViewModels;
+using LIC.Malone.Core;
+
+namespace LIC.Malone.Client.Desktop.DesignData
+{
+	public class AppViewModelDesignData : AppViewModel
+	{
+		public AppViewModelDesignData()
+		{
+			History = new BindableCollection<Request>(new HistoryDesignData());
+		}
+	}
+}
---
+++ LIC.Malone.Client.Desktop/DesignData/HistoryDesignData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using LIC.Malone.Core;
+using RestSharp;
+
+namespace LIC.Malone.Client.Desktop.DesignData
+{
+	public class HistoryDesignData : List<Request>
+	{
+		private static readonly IEnumerable<Request> FakeHistory = new List<Request>
+		{
+			new Request("http://api.openstreetmap.org/api/capabilities", Method.GET)
+			{
+				Response = new Response
+				{
+					HttpStatusCode = HttpStatusCode.OK
+				}
+			},
+			new Request("http://api.openstreetmap.org/api/0.6/changeset/create", Method.PUT)
+			{
+				Response = new Response
+				{
+					HttpStatusCode = HttpStatusCode.PaymentRequired
+				}
+			},
+			new Request("http://httpbin.org/get", Method.GET)
+			{
+				Response = new Response
+				{
+					HttpStatusCode = HttpStatusCode.OK
+				}
+			},
+			new Request("http://httpbin.org/get", Method.POST)
+			{
+				Response = new Response
+				{
+					HttpStatusCode = HttpStatusCode.MethodNotAllowed
+				}
+			}
+		};
+
+		public HistoryDesignData() : base(FakeHistory)
+		{
+		}
+	}
+}
