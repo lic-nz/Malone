f18c7aac3e2efd57387e00581ea4e4bb04411863
Shannon Skinner <shannonsnz@gmail.com>
2015-05-13 11:46:33 +1200

    Version bump

--- Solution Items/SharedAssemblyInfo.cs
+++ Solution Items/SharedAssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // http://blog.ploeh.dk/2013/12/10/semantic-versioning-with-continuous-deployment/
 // http://docs.nuget.org/create/nuspec-reference#Replacement_Tokens
 
-[assembly: AssemblyVersion("0.0.4")]
-[assembly: AssemblyFileVersion("0.0.4-alpha")]
-[assembly: AssemblyInformationalVersion("0.0.4-alpha")]
+[assembly: AssemblyVersion("0.0.5")]
+[assembly: AssemblyFileVersion("0.0.5-alpha")]
+[assembly: AssemblyInformationalVersion("0.0.5-alpha")]
