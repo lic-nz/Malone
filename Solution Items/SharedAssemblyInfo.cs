82d335396d1c4eddee90684b597682aa01405f2a
Brad Christensen <brad@christensen.co.nz>
2018-03-01 18:17:04 +1300

    Upgrade to .NET Framework 4.6 to fix TLS handshake with GitHub

--- Solution Items/SharedAssemblyInfo.cs
+++ Solution Items/SharedAssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // http://blog.ploeh.dk/2013/12/10/semantic-versioning-with-continuous-deployment/
 // http://docs.nuget.org/create/nuspec-reference#Replacement_Tokens
 
-[assembly: AssemblyVersion("0.9.6")]
-[assembly: AssemblyFileVersion("0.9.6")]
-[assembly: AssemblyInformationalVersion("0.9.6")]
+[assembly: AssemblyVersion("0.9.7")]
+[assembly: AssemblyFileVersion("0.9.7")]
+[assembly: AssemblyInformationalVersion("0.9.7")]
